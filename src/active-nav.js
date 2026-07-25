c851824e728c55492ad621e191d14473fcb0282c
w4serinn <you@example.com>
2026-07-25 09:42:42 +0900

    fix: active-nav を defer+IIFE に変更しfile://対応

--- src/active-nav.js
+++ src/active-nav.js
@@ -1,8 +1,10 @@
-let path = window.location.pathname;
-if (path.endsWith('/')) path += 'index.html';
+(function () {
+  var path = window.location.pathname;
+  if (path.endsWith('/')) path += 'index.html';
 
-document.querySelectorAll('.site-header__nav a').forEach((link) => {
-  if (new URL(link.href).pathname === path) {
-    link.classList.add('is-current');
-  }
-});
+  document.querySelectorAll('.site-header__nav a').forEach(function (link) {
+    if (new URL(link.href).pathname === path) {
+      link.classList.add('is-current');
+    }
+  });
+}());
